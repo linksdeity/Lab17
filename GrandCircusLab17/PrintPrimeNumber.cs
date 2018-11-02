ecbb0eed4ad481e948ed60cee2364bf2197004a9
Tim Broughton <TimBroughton@quickenloans.com>
2018-11-01 21:41:44 -0400

    initial commit, test code works, still needs main

---
+++ GrandCircusLab17/PrintPrimeNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandCircusLab17
+{
+    public class PrintPrimeNumber
+    {
+
+        public int UserInput { get; set; }
+
+
+        public int Output()
+        {
+            int counter = 1;
+            int numIncrement = 2;
+            int primeAnswer = 2;
+            bool primeChecker = true;
+
+            while (counter < UserInput)
+            {
+                primeChecker = true;
+
+                numIncrement++;
+
+                for (int i = 2; i < numIncrement; i++)
+                {
+                    if(numIncrement % i == 0)
+                    {
+                        primeChecker = false;
+                    }
+                }
+
+                if(primeChecker == true)
+                {
+                    primeAnswer = numIncrement;
+                    counter++;
+                }
+
+            }
+
+            return primeAnswer;
+        }
+
+
+
+
+    }
+}
---
+++ Lab17Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrandCircusLab17;
+
+
+namespace Lab17Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PrintPrimeNumber_PrintsOutCorrectPrimeNumber_true()
+        {
+
+            PrintPrimeNumber numberPrinter = new PrintPrimeNumber();
+
+            numberPrinter.UserInput = 10;
+
+            int output = numberPrinter.Output();
+
+            Assert.AreEqual(29 , output);
+
+        }
+    }
+}
